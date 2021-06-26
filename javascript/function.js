a94c02cd5e8bf9f06f2fab38f851ec001892b559
Lê Nguyễn Thành Nhân <74607205+lenhannhan123@users.noreply.github.com>
2021-06-26 21:50:21 +0700

    update

---
+++ javascript/function.js
@@ -0,0 +1,235 @@
+
+        function bangma1(str1){
+            let str2;
+        switch(str1){
+            case "a":{
+                str2= "😀";
+                break;
+            }
+            case "b":{
+                str2= "😃";
+                break;
+            }
+            case "c":{
+                str2= "😁";
+                break;
+            }
+            case "d":{
+                str2= "😅";
+                break;
+            }
+            case "e":{
+                str2= "🥰";
+                break;
+            }
+            case "f":{
+                str2= "🤣";
+                break;
+            }
+            case "g":{
+                str2= "😢";
+                break;
+            }
+            case "h":{
+                str2= "😰";
+                break;
+            }
+            case "i":{
+                str2= "😊";
+                break;
+            }
+            case "j":{
+                str2= "😇";
+                break;
+            }
+            case "k":{
+                str2= "😉";
+                break;
+            }
+            case "l":{
+                str2= "😒";
+                break;
+            }
+            case "m":{
+                str2= "😞";
+                break;
+            }
+            case "n":{
+                str2= "😙";
+                break;
+            }
+            case "o":{
+                str2= "😟";
+                break;
+            }
+            case "p":{
+                str2= "😕";
+                break;
+            }
+            case "q":{
+                str2= "🙂";
+                break;
+            }
+            case "r":{
+                str2= "🙃";
+                break;
+            }
+            case "s":{
+                str2= "☹️";
+                break;
+            }
+            case "t":{
+                str2= "😍";
+                break;
+            }
+            case "u":{
+                str2= "😳";
+                break;
+            }
+            case "v":{
+                str2= "😩";
+                break;
+            }
+            case "w":{
+                str2= "😭";
+                break;
+            }
+            case "x":{
+                str2= "😠";
+                break;
+            }
+            case "y":{
+                str2= "😋";
+                break;
+            }
+            case "z":{
+                str2= "😘";
+                break;
+            }
+
+            default:{
+                str2=str1;
+            }
+        }
+
+        return str2;
+        }
+
+
+        function bangma2(str1){
+            let str2;
+        switch(str1){
+            case "😀":{
+                str2= "a";
+                break;
+            }
+            case "😃":{
+                str2= "b";
+                break;
+            }
+            case "😁":{
+                str2= "c";
+                break;
+            }
+            case "😅":{
+                str2= "d";
+                break;
+            }
+            case "🥰":{
+                str2= "e";
+                break;
+            }
+            case "🤣":{
+                str2= "f";
+                break;
+            }
+            case "😢":{
+                str2= "g";
+                break;
+            }
+            case "😰":{
+                str2= "h";
+                break;
+            }
+            case "😊":{
+                str2= "i";
+                break;
+            }
+            case "😇":{
+                str2= "j";
+                break;
+            }
+            case "😉":{
+                str2= "k";
+                break;
+            }
+            case "😒":{
+                str2= "l";
+                break;
+            }
+            case "😞":{
+                str2= "m";
+                break;
+            }
+            case "😙":{
+                str2= "n";
+                break;
+            }
+            case "😟":{
+                str2= "o";
+                break;
+            }
+            case "😕":{
+                str2= "p";
+                break;
+            }
+            case "🙂":{
+                str2= "q";
+                break;
+            }
+            case "🙃":{
+                str2= "r";
+                break;
+            }
+            case "☹️":{
+                str2= "s";
+                break;
+            }
+            case "😍":{
+                str2= "t";
+                break;
+            }
+            case "😳":{
+                str2= "u";
+                break;
+            }
+            case "😩":{
+                str2= "v";
+                break;
+            }
+            case "😭":{
+                str2= "w";
+                break;
+            }
+            case "😠":{
+                str2= "x";
+                break;
+            }
+            case "😋":{
+                str2= "y";
+                break;
+            }
+            case "😘":{
+                str2= "z";
+                break;
+            }
+
+            default:{
+                str2=str1;
+            }
+        }
+
+        return str2;
+        }
+
+
